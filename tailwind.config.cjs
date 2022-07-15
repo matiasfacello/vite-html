2c789610c743758d19413011cf2cf1f021bf50e2
matiasfacello <matiasfacello@gmail.com>
2022-07-14 22:11:03 -0300

    Init

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{css,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig(() => {
+  return {
+    base: "./",
+    root: 'src',
+    build: {
+      outDir: '../dist'
+    }
+  }
+})
